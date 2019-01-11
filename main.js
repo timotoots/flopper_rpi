33686f5476ce25764c31457f0cff1786f6afa312
Timo <timo@timo.ee>
2019-01-11 13:49:52 +0200

    works through omxwebgui-v2

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const spawn = require( 'child_process' );
 var fs = require('fs');
 var colors = require('colors');
 
-
+var request = require('request');
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Serial
@@ -19,11 +19,15 @@ var cache_path = "/opt/flopper_rpi/cache/"
 
 var SerialPort = require('serialport');
 const Readline = SerialPort.parsers.Readline;
-
+try{
 var serialport = new SerialPort("/dev/ttyUSB0", {
   baudRate: 115200
 });
 
+} catch(e){
+  console.log("No Flopper Drive connected to /dev/ttyUSB0");
+  process.exit();
+}
 serialport.on('open', function(){
   console.log('Serial Port Opened');
  
@@ -41,6 +45,8 @@ var current_uid = "";
 
   var floppy_changed = 0;
   
+    data = trim(data);
+
    // var cmd = data.substr(0, 3); 
    if(data.substr(0, 8)=="Tag URL:"){
 
@@ -52,22 +58,23 @@ var current_uid = "";
 
       current_uid = trim(data.substr(9));   
 
-   } else if(data.substr(0, 20)=="Button pressed: NEXT"){
+   } else if(data.substr(0, 11)=="Touch: NEXT"){
       buttonPressed("next");
-   } else if(data.substr(0, 20)=="Button pressed: PREV"){
+   } else if(data.substr(0, 11)=="Touch: PREV"){
       buttonPressed("prev");
-   } else if(data.substr(0, 20)=="Button pressed: RANDOM"){
+   } else if(data.substr(0, 13)=="Touch: RANDOM"){
       buttonPressed("random");
-   } else if(data.substr(0, 20)=="Button pressed: PAUSE"){
-      buttonPressed("random");
-   } else if(data.substr(0, 20)=="Button pressed: REC"){
+   } else if(data.substr(0, 12)=="Touch: PAUSE"){
+      buttonPressed("pause");
+   } else if(data.substr(0,10 )=="Touch: REC"){
       buttonPressed("rec");
-   } else if(data.substr(0, 20)=="Button pressed: LIKE"){
+   } else if(data.substr(0, 11)=="Touch: LIKE"){
       buttonPressed("like");
-   } else if(data.substr(0, 20)=="Volume changed:"){
+   } else if(data.substr(0, 22)=="Potentiometer change: "){
 
-        var new_vol = trim(data.substr(15));   
+        var new_vol = trim(data.substr(22));   
         changeVolume(new_vol);
+        return false;
    } 
    
 
@@ -88,7 +95,9 @@ var current_uid = "";
 
    if (floppy_changed == 1 && floppy_url != ""){
 
+
       console.log("New floppy URL: " + floppy_url);
+      jump_to_track = 0;
       ParseUrl(floppy_url);
  
    }
@@ -107,44 +116,101 @@ function trim(str){
 
 function changeVolume(vol){
 
-  console.log("Change volume" + vol);
+  console.log("Change volume: " + vol);
+
+
+  const execSync = require('child_process').execSync;
+  var code = execSync("amixer sset 'PCM' "+vol+"%");
 
 
 }
 
+var buttonAccept = {"prev":1,"next":1,"random":1,"rec":1,"like":1,"pause":1};
+
 function buttonPressed(buttonId){
 
-  console.log("Button pressed:" + buttonId);
 
-  if(buttonId=="prev"){
+  if(buttonId=="prev" && buttonAccept.prev){
 
-    if(playlists.length>1 && current_track-1 < 0){
-      current_track--;
+    console.log("Button pressed:" + buttonId);
+
+    buttonAccept.prev = 0;
+    setTimeout(function(){
+      buttonAccept.prev = 1;
+    },3000);
+
+
+     if(playlists.length>1 && current_track-1 >= 0){
+      jump_to_track = current_track-1;
       checkPlayer();
     }
 
-  } else if(buttonId=="next"){
+  } else if(buttonId=="next" && buttonAccept.next){
+
+
+    console.log("Button pressed:" + buttonId);
+
+
+    buttonAccept.next = 0;
+    setTimeout(function(){
+      buttonAccept.next = 1;
+    },3000);
 
     if(playlists.length>1 && current_track+1 <= playlists.length-1){
-      current_track++;
+      jump_to_track = current_track+1;
       checkPlayer();
     }
 
-  } else if(buttonId=="random"){
+  } else if(buttonId=="random" && buttonAccept.random){
+
+   console.log("Button pressed:" + buttonId);
+
+
+    buttonAccept.random = 0;
+    setTimeout(function(){
+      buttonAccept.random = 1;
+    },3000);
 
     if(playlists.length>1){
-      current_track = Math.floor(Math.random() * playlists.length);
+      jump_to_track = Math.floor(Math.random() * (playlists.length-1));
       checkPlayer();
     }
 
 
-  } else if(buttonId=="pause"){
+  } else if(buttonId=="pause" && buttonAccept.pause){
+
+    console.log("Button pressed:" + buttonId);
+
+    buttonAccept.pause = 0;
+    setTimeout(function(){
+      buttonAccept.pause = 1;
+    },3000);
+
+    controlOmx("pause");
 
 
-  } else if(buttonId=="rec"){
+  } else if(buttonId=="rec" && buttonAccept.rec){
 
-  } else if(buttonId=="like"){
-    play_soundfile("/opt/flopper_rpi/sounds/like.mp3");
+    console.log("Button pressed:" + buttonId);
+
+
+    buttonAccept.rec = 0;
+    setTimeout(function(){
+      buttonAccept.rec = 1;
+    },3000);
+
+  } else if(buttonId=="like" && buttonAccept.like){
+
+    console.log("Button pressed:" + buttonId);
+
+
+    buttonAccept.like = 0;
+    setTimeout(function(){
+      buttonAccept.like = 1;
+    },3000);
+
+
+    play_soundfile("/opt/flopper_rpi/sounds/like.wav");
 
   } 
 
@@ -155,8 +221,23 @@ function buttonPressed(buttonId){
 
 var Sound = require('aplay');
 
-var MPlayer = require('mplayer'); 
-var player = new MPlayer();
+var MMPlayer = require('mplayer'); 
+var mplayer = new MMPlayer();
+
+// Import the module.
+// var omx = require('node-omxplayer');
+// var players = [];
+
+// players[0] = omx();
+// players[1] = omx();
+
+
+// Control video/audio playback.
+//player.pause();
+// player.volUp();
+//player.quit();
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -168,23 +249,110 @@ function play_soundfile(file){
 
   console.log("PLAY " + file);
 
-  player.openFile(file);
+  mplayer.openFile(file);
 
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var current_track = 0;
-
+var next_track = 0;
+var jump_to_track = -1;
+var current_player = 0;
 
 var playlists = [];
 
-function checkPlayer(){
+function controlOmx(cmd, params = ""){
 
-  console.log("CURRENT PLAYLIST".yellow);
-  console.log(playlists);
+  if(cmd=="status"){
+
+    request.post({url:'http://localhost:4321/index.php/index', form: {action:'dbus',command:'status'}}, function(err,httpResponse,body){ PlayerStatus(body) })
+    
+  } else if(cmd == "stop"){
+
+    request.post({url:'http://localhost:4321/index.php/index', form: {action:'shortcut',shortcut:'q'}}, function(err,httpResponse,body){ console.log("OMX Player stop!"); console.log(body) })
+    
+  } else if(cmd == "pause"){
+
+    request.post({url:'http://localhost:4321/index.php/index', form: {action:'shortcut',shortcut:'p'}}, function(err,httpResponse,body){ console.log("OMX Player pause!"); })
+    
+  } else if(cmd == "play"){
+
+    request.post({url:'http://localhost:4321/index.php/index', form: {action:'shortcut',shortcut:'start',path:params}}, function(err,httpResponse,body){ console.log("OMX Player play: "); console.log(body) })
+
+
+  }
 
 }
+
+function PlayerStatus(status){
+
+  status = JSON.parse(status);
+
+  if(typeof status != "undefined"){
+     if(typeof status.status != "undefined"){
+       if(typeof status.status.status != "undefined"){
+        console.log(status.status.status);
+       }
+      }
+  }
+    setTimeout(function(){ controlOmx("status"); }, 2000);
+
+}
+
+ controlOmx("status");
+
+function checkPlayer(){
+
+  
+
+  console.log("CHECK PLAYER".yellow);
+  console.log("Current track: " + current_track);
+  console.log("Jump to track: " + jump_to_track);
+  console.log("Current playlist".yellow);
+  console.log("Playlist length: " + playlists.length);
+
+  // no media, stop playing
+  if(playlists.length==0){
+
+     console.log("Omxplayer stop!");
+       controlOmx("stop");
+
+     // players[0].newSource("/opt/flopper_rpi/sounds/silence.wav");
+     // players[1].newSource("/opt/flopper_rpi/sounds/silence.wav");
+     // current_player[0].quit();
+     // current_player[1].quit();
+    
+
+  } else if(jump_to_track>=0){
+
+    if(typeof playlists[jump_to_track] === "undefined"){
+      console.log("Omxplayer no track found!");
+      jump_to_track = -1;
+    } else {
+      console.log("Omxplayer jump to  track:" + jump_to_track);
+      console.log(playlists[jump_to_track]);
+
+  
+      controlOmx("play",playlists[jump_to_track]);
+
+
+      current_track = jump_to_track;
+      jump_to_track = -1;
+    }
+
+  }
+
+  // if(next_track != current_track && playlists.length>0 || (next_track==0 && current_track==0) ){
+
+  //   player.newSource(playlists[next_track]);
+  //   player.play();
+  //   current_track = next_track;
+
+  // }
+
+
+} // checkPlayer
 
 
 function ParseUrl(url, playlist_start = 1, playlist_end=2){
@@ -196,20 +364,28 @@ function ParseUrl(url, playlist_start = 1, playlist_end=2){
 
     if(url=="https://vikerraadio.err.ee/"){
        url = "http://icecast.err.ee/vikerraadio.mp3.m3u";
-       url = "https://www.youtube.com/playlist?list=PLTkaXIuco-GPASgIwr00ALMth7UrjwD0i";
+    } else if(url=="http://www.radiorethink.com/tuner/?stationCode=wfmugtd&stream=hi"){
+      url  = "http://stream0.wfmu.org/drummer";
+    } else if(url == "http://www.radiorethink.com/tuner/?stationCode=wfmu&stream=hi"){
+      url = "http://stream0.wfmu.org/freeform-high.aac";
+             url = "https://www.youtube.com/playlist?list=PLTkaXIuco-GPASgIwr00ALMth7UrjwD0i";
        url = "https://soundcloud.com/tantsusaabas/tracks";
     }
+    
 
     if(typeof url === "undefined" || url==""){
       return false;
     }
 
-    var cmd = 'echo "'+ current_uid + '" && echo "'+ url +'" && echo "'+ playlist_start  +'" && echo "'+ playlist_end  +'"   && youtube-dl -g --quiet --no-warnings --ignore-errors -f best ';
-   
-    cmd = cmd + '--playlist-start '+ playlist_start +' --playlist-end ' + playlist_end;
- 
-    
-    cmd = cmd + ' "' + url + '"' ;
+    // Local youtube-dl
+    // var cmd = 'echo "'+ current_uid + '" && echo "'+ url +'" && echo "'+ playlist_start  +'" && echo "'+ playlist_end  +'"   && youtube-dl -g --quiet --no-warnings --ignore-errors -f best ';
+    // cmd = cmd + '--playlist-start '+ playlist_start +' --playlist-end ' + playlist_end;
+    // cmd = cmd + ' "' + url + '"' ;
+
+
+    var cmd = 'echo "'+ current_uid + '" && echo "'+ url +'" && echo "'+ playlist_start  + '" && echo "'+ playlist_end  + '" && wget -q -O - "http://play.flopper.net/youtubedl.php?cmd=urls&playlist_start=' + playlist_start + '&playlist_end=' + playlist_end + '&url= ' + url + '"';
+
+
 
     console.log("Youtube download started:");
     console.log(cmd.green);
